1b76c949923f9e5083a07b24783f0e5dc433408a
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2017-05-23 14:11:15 +0000

    Make bin/contraponto generate wav

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
 #include "MuMaterial.h"
 
 using namespace std;
 
+void ValidateArguments (int argc, char* argv[]);
+
 int main (int argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        cout << "Usage: " << argv[0] << " [score_file_path] [output_path]" << endl;
-        return -1;
-    }
+    ValidateArguments(argc, argv);
 
     string output_path = argc > 2 ? argv[2] : "./";
     string score_file_path = argv[1];
 
     MuInit();
-
     MuMaterial material;
-
     material.LoadScore(argv[1]);
 
     material.SetDefaultFunctionTables();
@@ -26,4 +23,14 @@ int main (int argc, char* argv[])
     material.Orchestra(output_path + "orchestra");
 
     return 0;
+}
+
+void ValidateArguments (int argc, char* argv[])
+{
+    bool has_arguments = argc < 2;
+    if (has_arguments)
+    {
+        cout << "Usage: " << argv[0] << " [score_file_path] [output_path]" << endl;
+        exit(-1);
+    }
 }
